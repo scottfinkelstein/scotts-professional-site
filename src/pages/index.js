2216839c6c2e46eafa1a1f8ef93601ee8c85cc1a
Scott Finkelstein <sbf0202@gmail.com>
2019-05-25 19:32:18 -0400

    Updated Metadata Keywords.

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ import agile_webm from "../images/Agile.webm"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`developer`, `ux`, `apps`, `web`, `leadership`, `technology`]} />
     <div className="main">
 
       <video loop muted autoPlay poster={ agile_jpg } className="agile-bg-video">
