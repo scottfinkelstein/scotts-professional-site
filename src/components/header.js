96d9a465de89b4cc22f65cc93102f54b40efe5b7
Scott Finkelstein <sbf0202@gmail.com>
2019-05-26 09:47:13 -0400

    Added a logo graphic, and reduced main div height.

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link, graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from "../images/scott-logo.svg"
 
 const Header = () => (
   <StaticQuery
@@ -20,7 +21,7 @@ const Header = () => (
     render={data => (
       <header>
         <nav>
-          <h1><Link to="/">Scott Finkelstein</Link></h1>
+          <h1><Link to="/"><img src={ logo } style={{ float: `left`, clear: `both`, width: `35px`, height: `35px`, marginRight: `3px`}} /> <span>Scott Finkelstein</span></Link></h1>
           <ul>
             { data.allGhostPage.edges.map(({ node }) => (
               <li><Link to={ node.slug }>{ node.title }</Link></li>
