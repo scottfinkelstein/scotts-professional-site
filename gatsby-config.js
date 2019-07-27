53493af301e938360ad846c2e2dd299646e8ec3f
Scott Finkelstein <sbf0202@gmail.com>
2019-07-27 17:20:43 -0400

    Edited gatsby-plugin-manifest properties.

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,17 +36,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `scottfinkelstein.com`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
